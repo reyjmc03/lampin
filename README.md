2baee6ad839d4c1bbc1a60633d5893b902990c6a
reyjmc03 <reyjmc03@gmail.com>
2016-02-28 15:37:47 +0800

    updated 02282016 1543 reyjmc03

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# lampin
+# LAMPIN
+Lamp Stack Server quick installation script
+
+# FEATURES
+- Add LAMP repositories
+- Remove LAMP repositories
+- Install LAMP(Linux, Apache, MySqL and PHP)
+
+# REQUIREMENTS
+- Python 2.7 or more
+- An operating system (tested on Ubuntu 14.00 latest versions must be developed soon.)
+
+# INSTALLATION
+- sudo su
+- git clone https://github.com/reyjmc03/lampin.git && cp lampin/lampin.py /usr/bin/lampin
+- chmod +x /usr/bin/lampin
+- sudo lampin
+
+# USAGE
+- Just select the number of a tool to install it
+- Press 0 to install all tools
+- back : Go back
+- gohome : Go to the main menu
+
+###### CHANGE LOGS
+**28 February 2016 15:34**
+- updated all python script
+
+# CONTACT
+- Website : http://www.jomarrey.com
+- Facebook : https://facebook.com/jomarrey03
+- Email : reyjmc03@gmail.com
+
+
--- lampin.py
+++ lampin.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
 
+# --------------------------------------------------------------------------------------------------
+# Developer : Jose Mari Caballa Rey
+# Project Name: lampin.py
+# Description: to develop a python based script install for lamp server installer for Ubuntu 14.00.
+# Version: 1.0
+# Date Started: 27 February 2016 
+# Date Updated: 28 February 2016
+# ---------------------------------------------------------------------------------------------------
+
 import os
 import sys, traceback
 
 def main():
-  try:
-    print '''  
-##################################################################
+	try:
+                print '''
 ##################################################################
 ------------------------------------------------------------------                      
-  @@         @@@      @@@@      @@@@   @@@@@@,   @@.  @@@'    @@ 
+  @@         @@@      @@@@      @@@@   @@@@@@,   @@.  @@@'    @@
   @@        '@@@+     @@@@     ,@@@@   @@@@@@@,` @@.  @@@@    @@ 
   @@        @@.@@     @@#@'    @@`@@   @@   @@@  @@.  @@@@@   @@ 
   @@        @@ @@`    @@ @@    @@`@@   @@   ;@@  @@.  @@`#@`  @@ 
@@ -21,27 +29,69 @@ def main():
   @@     `@@     @@`  @@   @@@#  `@@   @@        @@.  @@    @@@@ 
   @@@@@@ @@@     @@@  @@   @@@`  `@@   @@        @@.  @@   `@@@@ 
   @@@@@@ @@`     ,@@  @@   ;@@ ` `@@   @@        @@.  @@     @@@ 
+------------------------------------------------------------------
+##################################################################
 ------------------ LAMP Installer Command Script -----------------
------------------------------ v1.0 -------------------------------
 ##################################################################
-##################################################################
-'''
-    print '''
-          |-----------------------------------|
-          | Select the following:             |
-          | 1) Add Kali repositories & Update |
-          | 2) View Categories                |
-          | 3) Install classicmenu indicator  |
-          | 4) Install Kali menu              |
-          | 5) Help                           |
-          |-----------------------------------|
-'''
 
-  except KeyboardInterrupt:
-    print "@@@@@@@@@@@@@@@@@ Goodbye! @@@@@@@@@@@@@@@@@"
-  except Exception:
-    traceback.print_exc(file=sys.stdout)
-    sys.exit(0)
+		'''
+                def apache():
+                        print "apache ok!"
+                        switch_about = raw_input("\033[1;36mapache: select > \033[1;m")
+                        apache()
+                        
+                def mysql():
+                        print "mysql ok!"
+                        switch_about = raw_input("\033[1;36mmysql: select > \033[1;m")
+                        mysql()
+                        
+                def php():
+                        print "php ok!"
+                        switch_about = raw_input("\033[1;36mphpt: select > \033[1;m")
+                        php()
+                        
+                def about():
+                        print "about ok!"
+                        switch_about = raw_input("\033[1;36mabout: select > \033[1;m")
+                        about()
+                        
+		def mainmenu():
+			while True:
+				print '''
+|-----------------------------------|
+| Select the following:             |
+| 1) apt-get Update repositories    |
+| 2) Install or Remove Apache2      |
+| 3) Install or Remove MySql        |
+| 4) Install PHP                    |
+| 5) Help                           |
+| 6) About                          |
+|-----------------------------------|
+			'''
+				switch_main = raw_input("\033[1;36mmain: select > \033[1;m")
+			
+				if switch_main == "1":
+                                        command = os.system("apt-get update")
+                                elif switch_main == "2":
+                                        apache()
+                                elif switch_main == "3":
+                                        print "ok"
+                                elif switch_main == "4":
+                                        print "ok"
+                                elif switch_main == "5":
+                                        print " 5ok"
+                                elif switch_main == "6":
+                                        about()      
+                                else:
+                                        print "---- Invalid Command! ----"
+					
+		mainmenu()
+	except KeyboardInterrupt:
+		print "Shutdown requested...Goodbye..."
+	except Exception:
+		traceback.print_exc(file=sys.stdout)
+	sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
